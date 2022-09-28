a0d79a439bf01f1791cf99ba26dc72d92443808e
Milind Patel <milindpatel63@gmail.com>
2022-09-28 16:59:49 +0530

    feat(): test

--- stuff/install.sh
+++ stuff/install.sh
@@ -5,6 +5,9 @@ mkdir -p /etc/services.d/pihole-dot-doh
 
 # run file
 echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/run
+chown root:root /etc/services.d/pihole-dot-doh/run
+chmod 644 /etc/services.d/pihole-dot-doh/run
+chmod +x /etc/services.d/pihole-dot-doh/run
 # Copy config file if not exists
 echo 'cp -n /temp/stubby.yml /etc/pihole/' >> /etc/services.d/pihole-dot-doh/run
 echo 'cp -n /temp/cloudflared.yml /etc/pihole/' >> /etc/services.d/pihole-dot-doh/run
