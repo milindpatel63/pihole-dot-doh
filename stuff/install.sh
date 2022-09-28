8e06fdb8f6164bcb642b0caf389342a6e99006a4
Milind Patel <milindpatel63@gmail.com>
2022-09-28 17:40:43 +0530

    feat(): test4

--- stuff/install.sh
+++ stuff/install.sh
@@ -5,7 +5,8 @@ mkdir -p /etc/services.d/pihole-dot-doh
 chmod -R 777 /etc/services.d/pihole-dot-doh
 touch /etc/services.d/pihole-dot-doh/run
 # run file
-echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/run
+# echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/run
+echo '#!/usr/bin/env bash' > /etc/services.d/pihole-dot-doh/run
 # Copy config file if not exists
 echo 'cp -n /temp/stubby.yml /etc/pihole/' >> /etc/services.d/pihole-dot-doh/run
 echo 'cp -n /temp/cloudflared.yml /etc/pihole/' >> /etc/services.d/pihole-dot-doh/run
@@ -17,7 +18,7 @@ echo 's6-echo "Starting cloudflared"' >> /etc/services.d/pihole-dot-doh/run
 echo '/usr/local/bin/cloudflared --config /etc/pihole/cloudflared.yml' >> /etc/services.d/pihole-dot-doh/run
 
 # finish file
-echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/finish
+echo '#!/usr/bin/env bash' > /etc/services.d/pihole-dot-doh/finish
 echo 's6-echo "Stopping stubby"' >> /etc/services.d/pihole-dot-doh/finish
 echo 'killall -9 stubby' >> /etc/services.d/pihole-dot-doh/finish
 echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/pihole-dot-doh/finish
