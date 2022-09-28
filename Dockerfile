e1d9c9e3e9f5ef842712a71a4ee18deb9c4b10e6
Milind Patel <milindpatel63@gmail.com>
2022-09-28 13:15:11 +0530

    feat(): Updated config path to /etc/pihole

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # ARG FRM='testdasi/pihole-base-buster-plus'
-ARG FRM='testdasi/pihole-base-plus'
+ARG FRM='milindpatel63/pihole-base-plus'
 ARG TAG='latest'
 
 FROM ${FRM}:${TAG}
@@ -11,6 +11,6 @@ ADD stuff /temp
 RUN /bin/bash /temp/install.sh \
     && rm -f /temp/install.sh
 
-VOLUME ["/config"]
+VOLUME ["/etc/pihole"]
 
 RUN echo "$(date "+%d.%m.%Y %T") Built from ${FRM} with tag ${TAG}" >> /build_date.info
--- stuff/install.sh
+++ stuff/install.sh
@@ -6,14 +6,14 @@ mkdir -p /etc/services.d/pihole-dot-doh
 # run file
 echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/run
 # Copy config file if not exists
-echo 'cp -n /temp/stubby.yml /config/' >> /etc/services.d/pihole-dot-doh/run
-echo 'cp -n /temp/cloudflared.yml /config/' >> /etc/services.d/pihole-dot-doh/run
+echo 'cp -n /temp/stubby.yml /etc/pihole/' >> /etc/services.d/pihole-dot-doh/run
+echo 'cp -n /temp/cloudflared.yml /etc/pihole/' >> /etc/services.d/pihole-dot-doh/run
 # run stubby in background
 echo 's6-echo "Starting stubby"' >> /etc/services.d/pihole-dot-doh/run
-echo 'stubby -g -C /config/stubby.yml' >> /etc/services.d/pihole-dot-doh/run
+echo 'stubby -g -C /etc/pihole/stubby.yml' >> /etc/services.d/pihole-dot-doh/run
 # run cloudflared in foreground
 echo 's6-echo "Starting cloudflared"' >> /etc/services.d/pihole-dot-doh/run
-echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> /etc/services.d/pihole-dot-doh/run
+echo '/usr/local/bin/cloudflared --config /etc/pihole/cloudflared.yml' >> /etc/services.d/pihole-dot-doh/run
 
 # finish file
 echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/finish
