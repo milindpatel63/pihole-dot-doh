0fd315e07803b3787a59a51e874c39992443b4d1
Milind Patel <milindpatel63@gmail.com>
2022-09-30 07:35:44 +0530

    feat(): tunnel2

--- stuff/install.sh
+++ stuff/install.sh
@@ -16,8 +16,18 @@ echo 'stubby -g -C /etc/pihole/stubby.yml' >> /etc/services.d/pihole-dot-doh/run
 # run cloudflared in foreground
 echo 's6-echo "Starting cloudflared"' >> /etc/services.d/pihole-dot-doh/run
 echo '/usr/local/bin/cloudflared --config /etc/pihole/cloudflared.yml' >> /etc/services.d/pihole-dot-doh/run
+
+
 # cloudflared tunnel to access pihole webui securely
-echo '/usr/local/bin/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN' >> /etc/services.d/pihole-dot-doh/run
+mkdir -p /etc/services.d/cloudflaredtunnel/
+chmod -R 777 /etc/services.d/cloudflaredtunnel/
+touch /etc/services.d/cloudflaredtunnel/run
+# run file
+echo '#!/usr/bin/env bash' > /etc/services.d/cloudflaredtunnel/run
+chmod 777 /etc/services.d/cloudflaredtunnel/run
+# run cloudflared tunnel in foreground
+echo 's6-echo "Starting cloudflared tunnel"' >> /etc/services.d/cloudflaredtunnel/run
+echo '/usr/local/bin/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN' >> /etc/services.d/cloudflaredtunnel/run
 
 # finish file
 echo '#!/usr/bin/env bash' > /etc/services.d/pihole-dot-doh/finish
@@ -26,3 +36,9 @@ echo 's6-echo "Stopping stubby"' >> /etc/services.d/pihole-dot-doh/finish
 echo 'killall -9 stubby' >> /etc/services.d/pihole-dot-doh/finish
 echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/pihole-dot-doh/finish
 echo 'killall -9 cloudflared' >> /etc/services.d/pihole-dot-doh/finish
+
+# finish file
+echo '#!/usr/bin/env bash' > /etc/services.d/cloudflaredtunnel/finish
+chmod 777 /etc/services.d/cloudflaredtunnel/finish
+echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/cloudflaredtunnel/finish
+echo 'killall -9 cloudflared' >> /etc/services.d/cloudflaredtunnel/finish
