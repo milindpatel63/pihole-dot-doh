1bdcdfe858947a8712a3d512a4a03fa50ce786ac
Milind Patel <milindpatel63@gmail.com>
2023-01-09 20:36:52 +0530

    feat(): Update install.sh

--- stuff/install.sh
+++ stuff/install.sh
@@ -3,10 +3,10 @@
 # (/i insert above, /a insert below) #
 # Avoid pihole service due to the need to restart for every config change #
 # Insert run lines above the call lighttpd comment
-sed -i "/^lighttpd /i cp -n \/temp\/stubby.yml \/config/" /etc/s6-overlay/s6-rc.d/lighttpd/run
-sed -i "/^lighttpd /i cp -n \/temp\/cloudflared.yml \/config/" /etc/s6-overlay/s6-rc.d/lighttpd/run
-sed -i "/^lighttpd /i stubby -g -C \/config\/stubby.yml" /etc/s6-overlay/s6-rc.d/lighttpd/run
-sed -i "/^lighttpd /i start-stop-daemon --start --background --name cloudflared --chdir \/config --exec \/usr\/local\/bin\/cloudflared -- --config \/config\/cloudflared.yml" /etc/s6-overlay/s6-rc.d/lighttpd/run
+sed -i "/^lighttpd /i cp -n \/temp\/stubby.yml \/etc\/pihole/" /etc/s6-overlay/s6-rc.d/lighttpd/run
+sed -i "/^lighttpd /i cp -n \/temp\/cloudflared.yml \/etc\/pihole/" /etc/s6-overlay/s6-rc.d/lighttpd/run
+sed -i "/^lighttpd /i stubby -g -C \/etc\/pihole\/stubby.yml" /etc/s6-overlay/s6-rc.d/lighttpd/run
+sed -i "/^lighttpd /i start-stop-daemon --start --background --name cloudflared --chdir \/etc\/pihole --exec \/usr\/local\/bin\/cloudflared -- --config \/etc\/pihole\/cloudflared.yml" /etc/s6-overlay/s6-rc.d/lighttpd/run
 
 # Insert finish lines above kill
 sed -i "/^killall -9 lighttpd/i killall cloudflared" /etc/s6-overlay/s6-rc.d/lighttpd/finish
