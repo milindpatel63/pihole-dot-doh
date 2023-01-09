2d1f0bdb38d73833997c0c61625a0381e551e5e9
Milind Patel <milindpatel63@gmail.com>
2023-01-09 21:02:56 +0530

    feat(): update

--- stuff/install.sh
+++ stuff/install.sh
@@ -6,10 +6,10 @@
 sed -i "/^lighttpd /i cp -n \/temp\/stubby.yml \/etc\/pihole/" /etc/s6-overlay/s6-rc.d/lighttpd/run
 sed -i "/^lighttpd /i cp -n \/temp\/cloudflared.yml \/etc\/pihole/" /etc/s6-overlay/s6-rc.d/lighttpd/run
 sed -i "/^lighttpd /i stubby -g -C \/etc\/pihole\/stubby.yml" /etc/s6-overlay/s6-rc.d/lighttpd/run
-sed -i "/^lighttpd /i start-stop-daemon --start --background --name cloudflared --chdir \/etc\/pihole --exec \/usr\/local\/bin\/cloudflared -- --config \/etc\/pihole\/cloudflared.yml" /etc/s6-overlay/s6-rc.d/lighttpd/run
+sed -i "/^lighttpd /i start-stop-daemon --start --background --name cloudflared1 --chdir \/etc\/pihole --exec \/usr\/local\/bin\/cloudflared -- --config \/etc\/pihole\/cloudflared.yml" /etc/s6-overlay/s6-rc.d/lighttpd/run
 
 # Insert finish lines above kill
-sed -i "/^killall -9 lighttpd/i killall cloudflared" /etc/s6-overlay/s6-rc.d/lighttpd/finish
+sed -i "/^killall -9 lighttpd/i killall cloudflared1" /etc/s6-overlay/s6-rc.d/lighttpd/finish
 sed -i "/^killall -9 lighttpd/i killall stubby" /etc/s6-overlay/s6-rc.d/lighttpd/finish
 
 
